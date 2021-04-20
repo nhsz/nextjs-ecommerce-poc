4678093d9b7fd69a022ecd05f9b8b8b278c8acd4
nhsz <nh.quiroz@gmail.com>
2021-04-20 04:09:29 -0300

    chore: add /products pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,62 @@
+import { createClient } from 'contentful';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { FC } from 'react';
+import { ProductData } from '../components';
 
-export const Home = (): JSX.Element => (
-  <div>
-    <Head>
-      <title>Create Next App</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
+export const getStaticProps: GetStaticProps = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID ?? '',
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? ''
+  });
+  const results = await client.getEntries({ content_type: 'product' });
+  const products = results.items.map(result => result.fields);
 
-    <main>
-      <h1>e-commerce POC</h1>
-    </main>
+  return {
+    props: {
+      products
+    }
+  };
+};
 
+interface Products {
+  products: ProductData[];
+}
+
+export const Home: FC<Products> = ({ products }): JSX.Element => {
+  return (
     <div>
-      <a
-        href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Powered by <Image src='/vercel.svg' alt='Vercel Logo' height={'32'} width={'64'} />
-      </a>
+      <Head>
+        <title>NextJS e-commerce | Home</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+        <h1>e-commerce POC</h1>
+
+        {products.map((product: ProductData) => {
+          const { slug } = product;
+
+          return (
+            <div key={slug}>
+              <pre>{JSON.stringify(product, null, 2)}</pre>
+            </div>
+          );
+        })}
+      </main>
+
+      <div>
+        <a
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Powered by <Image src='/vercel.svg' alt='Vercel Logo' height={'32'} width={'64'} />
+        </a>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
