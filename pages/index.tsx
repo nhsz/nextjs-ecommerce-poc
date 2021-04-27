dc3508691c9d5082ac7862f31e3993e4e28ebc1e
nhsz <nh.quiroz@gmail.com>
2021-04-27 14:21:39 -0300

    fix: grid columns on smaller devices

--- pages/index.tsx
+++ pages/index.tsx
@@ -179,7 +179,7 @@ export const Home: FC<Products> = ({ products }): JSX.Element => {
               <Grid
                 templateColumns={{
                   base: 'repeat(1, 1fr)',
-                  md: 'repeat(2, 1fr)',
+                  lg: 'repeat(2, 1fr)',
                   xl: 'repeat(3, 1fr)'
                 }}
                 gap={{ base: 10, md: 8 }}
